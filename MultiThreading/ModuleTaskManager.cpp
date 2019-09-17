6756003d3ce297ec9e3f6370d7ab1a0e7430c309
GuillemD <gdrdvilla@gmail.com>
2019-09-17 12:12:02 +0200

    Added class example code

--- MultiThreading/ModuleTaskManager.cpp
+++ MultiThreading/ModuleTaskManager.cpp
@@ -40,3 +40,42 @@ void ModuleTaskManager::scheduleTask(Task *task, Module *owner)
 
 	// TODO 2: Insert the task into scheduledTasks so it is executed by some thread
 }
+
+
+//#include <stdlib.h>
+//#include <thread>
+//#include <iostream>
+//#include <windows.h>
+//#include <mutex>
+//
+//
+//
+//int counter = 0; // Global var
+//std::mutex mtx;
+//
+//void increment(int iterations) {
+//	for (int i = 0; i < iterations; ++i) {
+//		std::unique_lock<std::mutex> lock(mtx);
+//		counter++;
+//	}
+//}
+//int main() {
+//	std::thread t1(increment, 100000); // Execute 'increment' in a thread
+//	std::thread t2(increment, 100000); // Execute 'increment' in a thread
+//
+//	t1.join(); // Wait for t1
+//	t2.join(); // Wait for t2
+//	std::cout << "Counter = " << counter << std::endl;
+//
+//	system("pause");
+//	return 0;
+//}////void taskManagerMain(int arg1, int arg2, int arg3)//{
+//	while (true)
+//	{
+//		Task *t = nullptr;
+//
+//
+//		std::unique_lock<std::mutex> lock(mtx);
+//		while (t == nullptr)//		{////			if (scheduledTasks.empty())//			{//				cond.wait(lock);//			}//			else//			{//				t = scheduledTasks.front();//				scheduledTasks.pop();//			}//		}
+//	}
+////}
